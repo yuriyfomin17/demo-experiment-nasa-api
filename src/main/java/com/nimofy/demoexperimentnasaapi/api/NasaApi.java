e6eca9e43454f98f7611a939d17f4ce32dd6b807
nimofy1797 <yuriy.fomin@coro.net>
2023-03-13 20:41:26 +0000

    commit

--- src/main/java/com/nimofy/demoexperimentnasaapi/api/NasaApi.java
+++ src/main/java/com/nimofy/demoexperimentnasaapi/api/NasaApi.java
@@ -20,14 +20,26 @@ public class NasaApi {
     private final SpeedTracker speedTracker;
 
     @GetMapping("/compare/{sol}")
-    public ResponseEntity<Map<String, Stats>> compareDifferentMethods(@PathVariable int sol){
-        var statsCompletableFuture = speedTracker.getMaxImageCompletableFuture(sol);
-        var statsParallelStreams = speedTracker.getMaxImageParallelStream(sol);
-        var statsMaxImageReactive = speedTracker.getMaxImageReactive(sol);
-        Map<String, Stats> statsMap = new HashMap<>();
-        statsMap.put("COMPLETABLE_FUTURE", statsCompletableFuture);
-        statsMap.put("PARALLEL_STREAM", statsParallelStreams);
-        statsMap.put("REACTIVE_APPROACH", statsMaxImageReactive);
+    public ResponseEntity<Map<String, Double>> compareDifferentMethods(@PathVariable int sol){
+        double sumCompletableFutureTimeMillis = 0;
+        double sumParallelStreamTimeMillis = 0;
+        double sumReactiveTimeMillis = 0;
+        for (int i = 0; i < 20; i++) {
+            var statsCompletableFuture = speedTracker.getMaxImageCompletableFuture(sol);
+            sumCompletableFutureTimeMillis += statsCompletableFuture.timeTakenMillis();
+
+            var statsParallelStreams = speedTracker.getMaxImageParallelStream(sol);
+            sumParallelStreamTimeMillis += statsParallelStreams.timeTakenMillis();
+
+            var statsMaxImageReactive = speedTracker.getMaxImageReactive(sol);
+            sumReactiveTimeMillis += statsMaxImageReactive.timeTakenMillis();
+
+        }
+
+        Map<String, Double> statsMap = new HashMap<>();
+        statsMap.put("COMPLETABLE_FUTURE", sumCompletableFutureTimeMillis/ 20.0);
+        statsMap.put("PARALLEL_STREAM", sumParallelStreamTimeMillis / 20.0);
+        statsMap.put("REACTIVE_APPROACH", sumReactiveTimeMillis / 20.0);
         return new ResponseEntity<>(statsMap, HttpStatus.OK);
     }
 }
